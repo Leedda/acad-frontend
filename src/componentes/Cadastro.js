223483c1911242dc3879f490f96fc54edc25af8c
Ayres Flesch <ayresabf@gmail.com>
2017-10-15 19:45:28 -0200

    Máscaras e lista de estados

--- src/componentes/Cadastro.js
+++ src/componentes/Cadastro.js
@@ -56,11 +56,16 @@ class CadastroFormulario extends Component {
             this.setState({tipoCadastro: event.target.value});
         }.bind(this));
 
+        $('#estado').on('change', function (event) {
+            this.setState({estado: event.target.value});
+        }.bind(this));
+
         $('#restricoes').on('change', function (event) {
             this.setState({restricoes: [...event.target.options].filter(o => o.selected).map(o => o.value)});
         }.bind(this));
 
         $('#tipoCadastro').material_select();
+        $('#estado').material_select();
     }
 
     handleInputChange(event) {
@@ -137,8 +142,14 @@ class CadastroFormulario extends Component {
     }
 
     render() {
-        let im = new Inputmask("999.999.999-99");
-        im.mask($('#cpf'));
+        let maskCpf = new Inputmask("999.999.999-99");
+        let maskCep = new Inputmask("99999-999");
+        maskCpf.mask($('#cpf'));
+        maskCep.mask($('#cep'));
+        Inputmask({mask:  function () {
+            return ['(99) 9999-9999', '(99) 99999-9999'];
+        }, placeholder: " "}).mask($('#telefone'));
+
         return (
             <form className="col s6 offset-s3" onSubmit={this.enviaForm} method="POST">
                 {
@@ -185,9 +196,36 @@ class CadastroFormulario extends Component {
                 </div>
                 <div className="row">
                     <div className="input-field col s4">
-                        <input id="estado" name="estado" type="text"
-                               value={this.state.estado}
-                               onChange={this.handleInputChange} required/>
+                        <select id="estado" name="estado" value={this.state.estado}
+                                onChange={this.handleInputChange} required>
+                            <option value="AC">Acre</option>
+                            <option value="AL">Alagoas</option>
+                            <option value="AP">Amapá</option>
+                            <option value="AM">Amazonas</option>
+                            <option value="BA">Bahia</option>
+                            <option value="CE">Ceará</option>
+                            <option value="DF">Distrito Federal</option>
+                            <option value="ES">Espírito Santo</option>
+                            <option value="GO">Goiás</option>
+                            <option value="MA">Maranhão</option>
+                            <option value="MT">Mato Grosso</option>
+                            <option value="MS">Mato Grosso do Sul</option>
+                            <option value="MG">Minas Gerais</option>
+                            <option value="PA">Pará</option>
+                            <option value="PB">Paraíba</option>
+                            <option value="PR">Paraná</option>
+                            <option value="PE">Pernambuco</option>
+                            <option value="PI">Piauí</option>
+                            <option value="RJ">Rio de Janeiro</option>
+                            <option value="RN">Rio Grande do Norte</option>
+                            <option value="RS">Rio Grande do Sul</option>
+                            <option value="RO">Rondônia</option>
+                            <option value="RR">Roraima</option>
+                            <option value="SC">Santa Catarina</option>
+                            <option value="SP">São Paulo</option>
+                            <option value="SE">Sergipe</option>
+                            <option value="TO">Tocantins</option>
+                        </select>
                         <label htmlFor="estado">Estado</label>
                     </div>
                     <div className="input-field col s4">
